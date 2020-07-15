dc4fe721898bfc07e281cb560ce9017b97b3887d
richard jarram <rjarram@me.com>
2020-07-15 19:00:38 -0400

    separated parser into separate function

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-fn parse_config(args: &[String]) -> (&str, &str){
-    // &args[0] = /path/to/rust/binary/main.rs
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
-}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("searching for: {} in: {}...", query, filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
 
     println!("Text is:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str){
+    // &args[0] = /path/to/rust/binary/main.rs
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
