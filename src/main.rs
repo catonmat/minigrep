187d071b9a15c4b8bb06392d595a5a2d96d45497
richard jarram <rjarram@me.com>
2020-07-17 18:38:12 -0400

    redirected error messages to STD_ERR

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err_msg| {
-        println!("Error: {}", err_msg);
+        eprintln!("Error: {}", err_msg);
         process::exit(1);
     });
 
-    println!("searching for: {} in: {}...", config.query, config.filename);
+    println!("searching for `{}` in `{}`...\n", config.query, config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
