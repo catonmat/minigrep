000ab3484ca9cd72657499182c6d66143ad932f7
richard jarram <rjarram@me.com>
2020-07-17 17:42:32 -0400

    built test fort case sensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ mod tests {
     use super::*;
     
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
@@ -66,5 +66,20 @@ safe, fast, productive.
 Pick three.";
 
             assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUST";
+        let contents = "\
+Rust: 
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query,contents)
+        );
     }
 }
